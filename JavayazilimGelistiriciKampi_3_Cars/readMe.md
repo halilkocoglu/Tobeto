24a24c280e7da780c176f622be2f2e385cd67261
Halil ibrahim Kocoglu <halilkocoglu98@gmail.com>
2023-10-28 17:43:09 +0300

    Readme page added in Turkish.

---
+++ JavayazilimGelistiriciKampi_3_Cars/readMe.md
@@ -0,0 +1,10 @@
+# Java yazılım geliştirici kampı 27 Ekim ödevi
+
+Tobeto ve İstanbul kodluyor Canlı java dersi kapsamında oluşturuldu.
+Ödev kapsamında basit olarak bir e-ticaret sitesi ilişkisi baz alındı. 
+Bir araba modeline ait özellikler, kapsülleme ile oluşturuldu. Main üzerinde test edildi.
+
+- Business, Entities ve Data access paketleri oluşturuldu.
+- Interface ve class yapılarından faydalanıldı.
+- 3 basit method olan, create-update-delete methodları simüle edildi.
+- Veri tabanı erişim aracı olarak Jdbc ve Hibernate simüle edildi.
--- JavayazilimGelistiriciKampi_3_Cars/src/main/java/org/example/Main.java
+++ JavayazilimGelistiriciKampi_3_Cars/src/main/java/org/example/Main.java
@@ -2,13 +2,16 @@ package org.example;
 
 import org.example.business.CarManager;
 import org.example.dataAccess.HibernateCarDao;
+import org.example.dataAccess.JdbcCarDao;
 import org.example.entities.Car;
 
 public class Main {
     public static void main(String[] args) {
 
         Car car1 = new Car(1,"BMW","X5",1200000,15);
-        CarManager carManager =new CarManager(new HibernateCarDao());
+        CarManager carManager =new CarManager(new JdbcCarDao());
         carManager.add(car1);
+        carManager.delete(car1);
+        carManager.update(car1);
     }
 }
